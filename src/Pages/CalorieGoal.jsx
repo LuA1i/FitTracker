1c8631ddb4d11d8e648282d8cd9e119bbf4a6959
LuA1i <luqman.ali270304@outlook.com>
2024-07-16 00:02:00 +0100

    styling and finishing of code

--- src/Pages/CalorieGoal.jsx
+++ src/Pages/CalorieGoal.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
+import '../Components/CaloriGoalCSS/CalorieGoalCss.css'
 
 const CalorieGoal = () => {
-  const [height, useHeight] = useState(0)
-  const [weight, useWeight] = useState(0)
-  const [age, useAge] = useState('')
-  const [gender, useGender] = useState('')
-  const [activityLevel, setAcitivityLevel] = useState('level_1')
-  const [cal, setCal] = useState({ maintain: 0, gain: 0, lose: 0 })
+  const [height, setHeight] = useState(0)
+  const [weight, setWeight] = useState(0)
+  const [age, setAge] = useState(0)
+  const [gender, setGender] = useState('male')
+  const [activityLevel, setActivityLevel] = useState('level_1')
+  const [calories, setCalories] = useState({ maintain: 0, gain: 0, lose: 0 })
 
   const handleCalc = () => {
     let bmr = 0
@@ -16,80 +17,101 @@ const CalorieGoal = () => {
       bmr = 10 * weight + 6.25 * height - 5 * age - 161
     }
 
-    let acitivtyRate = 1.2
-
+    let activityMultiplier = 1.2
     switch (activityLevel) {
       case 'level_2':
-        acitivtyRate = 1.375
+        activityMultiplier = 1.375
         break
       case 'level_3':
-        acitivtyRate = 1.55
+        activityMultiplier = 1.55
         break
       case 'level_4':
-        acitivtyRate = 1.725
+        activityMultiplier = 1.725
         break
       case 'level_5':
-        acitivtyRate = 1.9
+        activityMultiplier = 1.9
+        break
       default:
-        acitivtyRate = 1.2
+        activityMultiplier = 1.2
     }
 
-    const maintanenceCalories = bmr * acitivtyRate
-
-    setCal({
-      maintain: Math.round(maintanenceCalories),
-      gain: Math.round(maintanenceCalories + 500),
-      lose: Math.round(maintanenceCalories - 500),
+    const maintenanceCalories = bmr * activityMultiplier
+    setCalories({
+      maintain: Math.round(maintenanceCalories),
+      gain: Math.round(maintenanceCalories + 500),
+      lose: Math.round(maintenanceCalories - 500),
     })
   }
   return (
     <div className="container">
       <h1>Calorie Goal</h1>
-      <div className="input-container">
-        <label>
-          Age (15+):
-          <input type="number" />
-        </label>
+
+      <div className="calorie-input-container">
+        <div className="input-container">
+          <label>
+            Age (15+) :
+            <input
+              type="number"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="input-container">
+          <label htmlFor="Gender">
+            Gender:
+            <select
+              id="gender-selection"
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+            >
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+          </label>
+        </div>
+        <div className="input-container">
+          <label>
+            Height (cm):
+            <input
+              type="number"
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="input-container">
+          <label>
+            Weight (kg):
+            <input
+              type="number"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="input-container">
+          <label>
+            Activity Level:
+            <select
+              value={activityLevel}
+              onChange={(e) => setActivityLevel(e.target.value)}
+            >
+              <option value="level_1">Sedentary</option>
+              <option value="level_2">Lightly active</option>
+              <option value="level_3">Moderately active</option>
+              <option value="level_4">Very active</option>
+              <option value="level_5">Extra active</option>
+            </select>
+          </label>
+        </div>
+        <button onClick={handleCalc}>Calculate</button>
       </div>
-      <div className="input-container">
-        <label htmlFor="Gender">
-          Gender:
-          <select id="gender-selection">
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-          </select>
-        </label>
-      </div>
-      <div className="input-container">
-        <label>
-          Height (cm):
-          <input type="number" />
-        </label>
-      </div>
-      <div className="input-container">
-        <label>
-          Weight (kg):
-          <input type="number" />
-        </label>
-      </div>
-      <div className="input-container">
-        <label>
-          Activity Level:
-          <select>
-            <option value="level_1">Sedentary</option>
-            <option value="level_2">Lightly active</option>
-            <option value="level_3">Moderately active</option>
-            <option value="level_4">Very active</option>
-            <option value="level_5">Extra active</option>
-          </select>
-        </label>
-      </div>
-      <button>Calculate</button>
       <div>
         <h2>Calorie Goals</h2>
-        <p>Maintain weight: calories/day</p>
-        <p>Gain weight: calories/day</p>
-        <p>Lose weight: calories/day</p>
+        <p>Maintain weight: {calories.maintain} calories/day</p>
+        <p>Gain weight: {calories.gain} calories/day</p>
+        <p>Lose weight: {calories.lose} calories/day</p>
       </div>
     </div>
   )
