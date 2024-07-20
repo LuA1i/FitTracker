9a308f03e8f0b7e2551b4dc6ffd83458daadc6be
LuA1i <luqman.ali270304@outlook.com>
2024-07-20 20:36:32 +0100

    .

--- src/Pages/CalorieTracker.jsx
+++ src/Pages/CalorieTracker.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import '../Components/CalorieTracker/calorieTracker.css'
 const CalorieTracker = () => {
   return (
-    <div>CalorieTracker</div>
+    <div>
+      <div className="container">
+        <h1>CalorieGoal: </h1>
+        <h2>Current Calorie Intake: </h2>
+        <div className="userinput">
+          <label htmlFor=""> Add Food: </label>
+          <input className="food-item" type="text" />
+          <label htmlFor="">Cal: </label>
+          <input className="calories" type="number" />
+          <button className="add-food">Add</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
